90aec62c86084ec97cf03064596915a5c7f6d558
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2023-03-09 13:31:24 +0100

    updated labels

--- pages/admin/list/location.vue
+++ pages/admin/list/location.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mt-3">
         <b-button variant="primary" @click="showAddLocationModal = !showAddLocationModal">Add Location</b-button>
-        <b-table :items="computedLocationList" :fields="fields" :busy="isLoading" ref="location" class="mt-4" striped hover>
+        <b-table :items="computedLocationList" :fields="fields" :busy="isLoading" ref="location" class="mt-4" striped hover outlined>
             <template #cell(actions)="row">
                 <div class="d-flex justify-content-around">
                     <b-button @click="handleSelectedLocation(row.item)" variant="primary">Edit</b-button>
@@ -24,8 +24,8 @@
                 <b-form-group label="Shortcode" label-for="shortcode">
                     <b-form-input id="shortcode" v-model="newLocation.shortcode" minlength="3" required></b-form-input>
                 </b-form-group>
-                <b-button type="submit" variant="primary">Add Location</b-button>
-                <b-button type="button">Cancel</b-button>
+                <b-button type="submit" variant="primary" class="mr-3">Add Location</b-button>
+                <b-button type="button" @click="showAddLocationModal = !showAddLocationModal">Cancel</b-button>
             </b-form>
         </b-modal>
 
@@ -37,7 +37,8 @@
                 <b-form-group label="Shortcode" label-for="edit-shortcode">
                     <b-form-input id="edit-shortcode" v-model="selectedLocation.shortcode" minlength="3" required></b-form-input>
                 </b-form-group>
-                <b-button type="submit" variant="primary">Save Location</b-button>
+                <b-button type="submit" variant="primary" class="mr-3">Save Location</b-button>
+                <b-button type="button" @click="showUpdateLocationModal = !showUpdateLocationModal">Cancel</b-button>
             </b-form>
         </b-modal>
     </div>
@@ -149,7 +150,7 @@ export default {
                 this.showUpdateLocationModal = false
                 this.handleGetLocationList();
             }).catch((error) => {
-                this.$bvToast.toast(error?.response?.data, {
+                this.$bvToast.toast(error?.response?.data.message, {
                     title: 'Error',
                     variant: 'danger',
                     delay: 300
@@ -180,7 +181,7 @@ export default {
                 });
                 }
             }).catch(err => {
-                this.$bvToast.toast(err?.response?.data, {
+                this.$bvToast.toast(err?.response?.data?.message, {
                     title: 'Error',
                     variant: 'danger',
                     delay: 300
