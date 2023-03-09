7b52b7e0bfa7b2668bea6f910f6b0b967128a7c2
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2023-03-09 13:30:47 +0100

    added filter

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/axios'
+    '~/plugins/axios',
+    '~plugins/filters'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
