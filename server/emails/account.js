9fcc90148fd2eb1bf0a1a342921dbe415e25230e
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2023-03-10 13:24:04 +0100

    updated account mail file

--- server/emails/account.js
+++ server/emails/account.js
@@ -1,15 +1,25 @@
 const sgMail = require('@sendgrid/mail')
 
 const sendgridAPIKey = process.env.SENDGRID_API_KEY;
+const senderEmail = process.env.EMAIL_ADDRESS_OWNER;
 
 sgMail.setApiKey(sendgridAPIKey)
 
 const sendWelcomeMessage = (email, name) => {
     sgMail.send({
         to: email, //temporary
-        from: 'andrew@mead.io',
+        from: process.env.EMAIL_ADDRESS_OWNER,
         subject: 'This is my first creation!',
         text: `Hello ${name} I hope this one actually get to you`
+    })
+}
+
+const sendTicketInfo = (email, ticket_id, amount) => {
+    sgMail.send({
+        to: email, //temporary
+        from: senderEmail,
+        subject: 'NMBTS Ticket Information',
+        text: `Dear Customer, your ticket id is ${ticket_id} and the amount due is ${amount}`
     })
 }
 
@@ -17,7 +27,7 @@ const sendWelcomeMessage = (email, name) => {
 const sendCancellationMessage = (email, name) => {
     sgMail.send({
         to: email, //temporary
-        from: 'andrew@mead.io',
+        from: process.env.EMAIL_ADDRESS_OWNER,
         subject: 'So sad to let you go',
         text: `Hello ${name} I hope this one actually get to you`
     })
@@ -25,5 +35,6 @@ const sendCancellationMessage = (email, name) => {
 
 module.exports = {
     sendWelcomeMessage,
-    sendCancellationMessage
+    sendCancellationMessage,
+    sendTicketInfo
 }
