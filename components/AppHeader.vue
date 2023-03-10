e9d3ed64001e47110509a6ab79e319dfef5cfdbb
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2023-03-10 08:35:11 +0100

    added home link to nav

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,8 +4,9 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav class="ml-auto">
-                <b-nav-item to="/admin" exact exact-active-class="active">Admin</b-nav-item>
-                <b-nav-item to="/contact" exact exact-active-class="active">Contact Us</b-nav-item>
+                <b-nav-item to="/" exact exact-active-class="active">Home</b-nav-item>
+                <b-nav-item to="/admin" active-class="active">Admin</b-nav-item>
+                <b-nav-item to="/contact" active-class="active">Contact Us</b-nav-item>
             </b-navbar-nav>
         </b-collapse>
         <b-button v-if="isAuthenticated" @click="handleAdminLogout()">Logout</b-button>
