0ed190fdf525850ae0c13456ca02d834e4f7d15f
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2023-03-10 13:23:13 +0100

    updated the contact us page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
-
-    </div>
+    <b-container>
+        <b-jumbotron class="my-5" header="Contact Us" lead="Please feel free to contact us using the email address below:">
+            <p><strong>Email:</strong> <a href="mailto:test@gmail.com">test@gmail.com</a></p>
+        </b-jumbotron>
+    </b-container>
 </template>
 
 <script>
     export default {
-        
+        layout: "admin"
     }
 </script>
 
